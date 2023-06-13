5c569f4f0dda6f8e4c622d86d5924b11cc5ec971
David Jakowenko <jakowenko@gmail.com>
2023-06-13 03:21:22 -0400

    fix: ensure summary is defined before creating

--- api/src/events/summary.event.ts
+++ api/src/events/summary.event.ts
@@ -2,11 +2,7 @@ import prisma from '../util/prisma.util';
 import { emitter } from '../util/emitter.util';
 
 emitter.on('summary.create', async (summary: string) => {
-  const data = await prisma.summary.create({
-    data: {
-      summary,
-    },
-  });
-
+  if (!summary) return;
+  const data = await prisma.summary.create({ data: { summary } });
   emitter.emit('image.create', data);
 });
