8e21704f511f23a248a1d705650d5d4997836d96
David Jakowenko <jakowenko@gmail.com>
2023-06-20 13:47:38 -0400

    refactor: use generic bytes to size util

--- api/src/controllers/logs.controller.ts
+++ api/src/controllers/logs.controller.ts
@@ -3,19 +3,13 @@ import express from 'express';
 
 import config from '../config';
 import readLastLines from 'read-last-lines';
+import { bytesToSize } from '../util/size.util';
 
 const router = express.Router();
 
 const {
   SYSTEM: { STORAGE },
 } = config();
-
-const bytesToSize = (bytes: number) => {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return '0 Bytes';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString(), 10);
-  return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
-};
 
 router.get('/', async (req, res) => {
   const { size } = fs.statSync(`${STORAGE.PATH}/messages.log`);
